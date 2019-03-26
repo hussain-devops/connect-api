ddc2fce3717b52c84c02d157b6caf85b767e85a3
hussain-devops <hussain.devops12@gmail.com>
2019-03-26 23:20:05 +0530

    Initial Commit with Codebase

---
+++ src/main/java/com/hussain/app/connectapi/ConnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.hussain.app.connectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectApiApplication.class, args);
+	}
+
+}
